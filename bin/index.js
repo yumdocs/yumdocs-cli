fd297fc345b6f4edc29db80626f0540dc715e532
jlchereau <jlchereau@msn.com>
2022-10-25 20:13:10 +0200

    Added yargs

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-require("../lib/index.js")(process.argv);
+require('yargs')
+    .scriptName('yumdocs')
+    .usage('$0 <cmd> [args]')
+    .command(
+        'hello [name]',
+        'welcome ter yargs!',
+        (yargs) => {
+            yargs.positional('name', {
+                type: 'string',
+                default: 'Cambi',
+                describe: 'the name to say hello to'
+            })
+        },
+        require('../lib/index.js')
+    )
+    .help()
+    .argv;
